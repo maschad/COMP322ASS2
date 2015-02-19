4f16c17ece232bc2072cdadb1c5a79c0f8e15d2e
cnehem <cnehem@open-13.CS.McGill.CA>
2015-02-19 18:41:55 -0500

    Question 3 done

--- CS322_HW2/src/flowersStudent.cpp
+++ CS322_HW2/src/flowersStudent.cpp
@@ -1,7 +1,22 @@
 #include "flowers.h"
 
 unsigned int spitEulerSquare(unsigned int index) {
-    // TODO : add code here
+	if(index ==0)return 7;
+	if(index ==1) return 2;
+	if(index%5 == 3 || index%5 == 2) return 1;
+	if(index%5 == 0)
+	{
+		return 6 + (index/5)*12;
+	}
+	if(index%6 == 0)
+	{
+		return 2 + (index/6)*3;
+	}
+	if(index%4 == 0)
+	{
+		return (index/4)*3;
+	}
+
 }
 
 Fraction getApproximation(ContinuedFraction &fr, unsigned int n){
