ee2ebe105375b1d35db4a57cc3a8fd664f9da4cd
cnehem <cnehem@lab6-8.CS.McGill.CA>
2015-02-28 16:26:27 -0500

    #4 done I think

--- CS322_HW2/src/flowersStudent.cpp
+++ CS322_HW2/src/flowersStudent.cpp
@@ -2,7 +2,7 @@
 
 unsigned int spitEulerSquare(unsigned int index) {
 	if(index ==0)return 7;
-	if(index ==1) return 2;
+	if(index == 1) return 2;
 	if(index%5 == 3 || index%5 == 2) return 1;
 	if(index%5 == 0)
 	{
@@ -19,13 +19,42 @@ unsigned int spitEulerSquare(unsigned int index) {
 
 }
 
-Fraction getApproximation(ContinuedFraction &fr, unsigned int n){
-    // TODO : add code here
+ContinuedFraction ignoreInt(ContinuedFraction f)
+{
+	f.fixedPart[0] = 0;
+	return f;
 }
 
+Fraction getApproximation(ContinuedFraction &fr, unsigned int n){
+
+	fr = ignoreInt(fr);
+
+    Fraction y;
+    y.numerator= fr.fixedPart[1];
+    y.denominator = 1;
+
+    if(n == 1 || fr.fixedPart[1] == 0)
+    {
+    	return y;
+    }
+    else
+    {
+		Fraction x = getApproximation(fr,n-1);
+		Fraction temp;
+		temp.numerator = x.numerator;
+		temp.denominator = x.denominator;
+		x.denominator = temp.numerator;
+		x.numerator = temp.denominator;
+		x.numerator = x.numerator + x.denominator*fr.fixedPart[0];
+		return x;
+    }
+
+}
 
 double getAngle(ContinuedFraction &theta, int k) {
-    // TODO : add code here
+   Fraction frac = getApproximation(theta,7);
+
+   return (frac.numerator*k)/(frac.denominator);
 }
 
 Seed getSeed(ContinuedFraction &theta, int k) {
@@ -44,8 +73,4 @@ ContinuedFraction getCFUsingMB(ContinuedFraction &f, int a, int b, int length) {
     // TODO : add code here
 }
 
-ContinuedFraction ignoreInt(ContinuedFraction f)
-{
-	f.fixedPart[0] = 0;
-	return f;
-}
+
